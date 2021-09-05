08d2615c293aa1bd334e99dcd6fca31aefe9644d
Pavaman Dabeer <pavamandabeer@gmail.com>
2021-09-05 16:56:55 +0530

    reset password route functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -82,8 +83,30 @@ exports.forgotPassword = async (req, res, next) => {
   }
 };
 
-exports.resetPassword = (req, res, next) => {
-  res.send("Reset password route");
+exports.resetPassword = async (req, res, next) => {
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
+  try {
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordTokenExpiry: { $gt: Date.now() },
+    });
+    if (!user) {
+      return next(new ErrorResponse("Invalid reset Token", 400));
+    }
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTokenExpiry = undefined;
+    user.save();
+    res.status(200).json({
+      success: true,
+      data: "Password reset successful",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const sendToken = (user, statusCode, res) => {
